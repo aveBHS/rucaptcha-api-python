1bc3f60cf05c886a62af3ea0369669514e4cb6cd
aveBHS <aveBHS.ru@gmail.com>
2020-01-28 17:09:13 +0300

    Основа модуля
    Создание и инициализация основных классов

---
+++ rucaptcha.py
@@ -0,0 +1,13 @@
+import urllib.request
+import requests
+
+class RuCaptchaConnection:
+    token = ''
+    def __init__(token):
+        self.token = token
+
+class RuCaptcha:
+    cid = ''
+    def __init__(cid):
+        self.cid = cid   
+
