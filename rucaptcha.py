7f4c470812842815f33429ee46db69a468c01f59
aveBHS <aveBHS.ru@gmail.com>
2020-01-28 19:24:19 +0300

    Новые функции
    1.Запрос готовности капчи
    2.Получение ответа на капчу
    3.Ожидание решения капчи

--- rucaptcha.py
+++ rucaptcha.py
@@ -1,8 +1,9 @@
 import urllib.request
 import requests
+import time
 
 class RuCaptchaConnection:
-    token = ''
+    token = None
     def __init__(self, token):
         self.token = token
 
@@ -19,9 +20,49 @@ class RuCaptchaConnection:
             raise ValueError(f'[{response.status_code}] Could not connect to RuCatcha server!')
 
 class RuCaptcha:
-    cid = ''
-    token = ''
+    cid = None
+    token = None
+    decision = None
     def __init__(self, cid, token):
         self.token = token
         self.cid = cid   
 
+    def captcha_ready(self):
+        data = {"key" : self.token, 'action': 'get', 'id': self.cid}
+        response = requests.post("https://rucaptcha.com/res.php", data=data)
+        if(response.text.split('|')[0].upper() == "OK"):
+            return True
+        elif(response.text == "CAPCHA_NOT_READY"):
+            self.decision = response.text.split('|')[0]
+            return False
+        else:
+            raise ValueError(response.text)
+    
+    def get_decision(self):
+        if(self.decision == None):
+            data = {"key" : self.token, 'action': 'get', 'id': self.cid}
+            response = requests.post("https://rucaptcha.com/res.php", data=data)
+            if(response.text.split('|')[0].upper() == "OK"):
+                self.decision = response.text.split('|')[1]
+                return self.decision
+            elif(response.text == "CAPCHA_NOT_READY"):
+                raise ValueError(response.text)
+            else:
+                raise ValueError(response.text)
+        else: 
+            return self.decision
+    
+    def wait_decision(self):
+        waited = 0
+        data = {"key" : self.token, 'action': 'get', 'id': self.cid}
+        while True:
+            if(waited >= 60):
+                raise ValueError('Timeout waiting decision')
+            response = requests.post("https://rucaptcha.com/res.php", data=data)
+            if(response.text.split('|')[0].upper() == "OK"):
+                self.decision = response.text.split('|')[1]
+                return self.decision
+            else:
+                time.sleep(2)
+                waited += 5
+                continue
